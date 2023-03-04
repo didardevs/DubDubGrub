cb0edcd59337ccd673c5900737779185cf067bfb
Didar Naurzbayev <didardevs@gmail.com>
2023-03-04 11:32:28 -0800

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Didar Naurzbayev on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
